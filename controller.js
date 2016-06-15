45eca1d69aaf8cedb49fe55e7b44c664f8b4623f
karthik chandrashekar <karthik.c@lnttechservices.com>
2016-06-15 12:59:38 +0530

    added service and factory

--- controller.js
+++ controller.js
@@ -1,10 +1,43 @@
 var app = angular.module('calculatorApp', []);
+
+
+app.service('rihannaService',function(){
+
+  this.getTopRatedAlbum = function(){
+    return 'Monster ft eminem'
+  }
+
+});
+
+
+app.factory('lucianaFactory',function(){
+  var albums = {};
+
+  albums.topRated = function(){
+    return 'Jhon Legend Cover'
+  }
+  return albums;
+});
+
 app.controller('myCtrl', function($scope) {
   $scope.sum = function(){
-    console.log('sum');
     var result =  $scope.a + $scope.b;
     console.log('result of adding a and b :', result);
     alert('result :'+result);
     return result;
   }
+});
+
+app.controller('lucianaCtrl', function($scope,lucianaFactory,rihannaService) {
+
+  $scope.callLucianaFactory = function(){
+    var topRated = lucianaFactory.topRated();
+    console.log('top rated song is :',topRated);
+  }
+
+  $scope.callRihannaService = function(){
+    var topRated = rihannaService.getTopRatedAlbum();
+    console.log('Top rated rihanna service is :',topRated);
+  }
+
 });
